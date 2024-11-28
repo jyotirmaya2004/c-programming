e7d151aa6e8172fa8f91ab98138e268bb73d9da4
jyotirmaya2004 <jyotirmaybehera2020@mail.com>
2024-11-28 20:14:22 +0530

    28/11/2024

--- Class Assignment/string 2/q13_concatenate_1st_with_reverse_of_2nd.c
+++ Class Assignment/string 2/q13_concatenate_1st_with_reverse_of_2nd.c
@@ -1,4 +1,27 @@
 #include<stdio.h>
+#include<string.h>
+void reverseWords(char str[]) {
+    int n = strlen(str);
+    char temp[200];
+    int i, j = 0, k = 0;
+
+    // Reverse the entire sentence
+    for (i = n - 1; i >= 0; i--) {
+        if (str[i] == ' ' || i == 0) {
+            // Add the word to the temp array
+            int start = (i == 0) ? i : i + 1;
+            while (str[start] != ' ' && str[start] != '\0') {
+                temp[j++] = str[start++];
+            }
+            // Add a space after the word
+            if (k < n - 1) temp[j++] = ' ';
+        }
+    }
+    temp[j] = '\0'; // Null-terminate the reversed string
+
+    // Copy the reversed string back to the original
+    strcpy(str, temp);
+}
 int main()
 {
     char a[100],b[50],c[50],d[50];
@@ -13,10 +36,14 @@ int main()
             c[j++]=b[i];
         else
         {
-            c[j]='\0';
+            c[j]=' ';
+            c[j+1]='\0';
             strcat(d,c);
             j=0;
         }
     }
+    reverseWords(d);
+    strcat(a,d);
+    puts(a);
 
 }
