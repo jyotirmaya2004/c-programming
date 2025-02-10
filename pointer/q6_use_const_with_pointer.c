c6bfc203dea88cd7f6b98c119f20f92a7c615102
jyotirmaya2004 <jyotirmaybehera2020@mail.com>
2025-02-10 18:03:37 +0530

    a

--- pointer/q6_use_const_with_pointer.c
+++ pointer/q6_use_const_with_pointer.c
@@ -2,7 +2,7 @@
 int main() {
     const int a = 10;
     const int *p = &a; // Pointer to a constant
-    //*p = 20; // Error: Cannot modify value through pointer
+    // *p = 20; // Error: Cannot modify value through pointer
     printf("%d\n", *p);
     return 0;
 }
